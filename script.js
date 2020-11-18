b1aad55ae18dfee942ab32410d76231d0e6a56b1
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2020-11-18 15:48:05 -0500

    Added password length validation

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// function to determine what the password criteria are and return the possible character for generation
 function getCriteria(){
   var num = confirm("Press Confirm if you would like your password to contain numbers.");
   var lower = confirm("Press Confirm if you would like lower case letters in your password");
@@ -46,23 +47,33 @@ function getCriteria(){
  return (finalString);
  
 }
-
+// function that takes the criteria and creates the password string
 function generatePassword(passwordLength,finalString) {
  var result = "";
   for(var i = 0; i < passwordLength; i++){ 
-var random = Math.floor(Math.random() * finalString.length);
-   result += finalString.charAt(random);
+    var random = Math.floor(Math.random() * finalString.length);
+    result += finalString.charAt(random);
   }
-   console.log(result);
   return(result);
 
 }
-
+var lengthValid = false;
 // Prompt for Password Length
+do{
 var passwordLength = prompt("How long would you like your password to be? It can be 8 to 128 char long.");
 var length = parseInt(passwordLength, 10);
+ if (length < 8){
+   alert("Your password needs to be at least 8 characters.");
+ }
+ else if (length > 128) {
+   alert("Your password needs to be 128 characters or less.");
+ }
+ else{
+   lengthValid = true;
+ }
+ } while (!lengthValid);
 
-// Call the functions to generate the password
+// Call the functions to collect criteria and generate the password
 getCriteria();
 if (finalString === "") {
   getCriteria();
